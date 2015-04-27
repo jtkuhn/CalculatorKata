b1601d2225901b9bf6206b8e6f0e85c2dcc01ade
jtkuhn <jtkuhn@foxguardsolutions.com>
2015-04-27 10:19:09 -0400

    Works perfectly for any amount of number separated by commmas (Steps 1,2)

--- CalculatorKata/Calculator.cs
+++ CalculatorKata/Calculator.cs
@@ -1,13 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CalculatorKata
 {
     public class Calculator
     {
+        private int sum = 0;
+
+        public int Add(String numbers)
+        {
+            sum = 0;
+
+            if (numbers.Length == 1 || numbers.Length == 0)
+            {
+                return SumOfZeroOrOneNumbers(numbers);
+            }
+
+            while (numbers.Contains(","))
+            {
+                numbers = GetNextNumber(numbers);
+            }
+            sum += Convert.ToInt16(numbers);
+
+            return sum;
+        }
+
+
+
+        private string GetNextNumber(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            int pos = 0;
+
+
+            while (s.Substring(pos, 1) != ",")
+            {
+                sb.Append(s.Substring(pos, 1));
+                pos++;
+            }
+            sum += Convert.ToInt16(sb.ToString());
+            sb.Clear();
+            return s.Substring(pos + 1);
+        }
+
+        private int SumOfZeroOrOneNumbers(string s)
+        {
+            if (s.Length == 1)
+            {
+                return Convert.ToInt16(s);
+            }
+            else return 0;
+        }
+
 
     }
 }
+    
--- UnitTestProject1/CalculatorTest.cs
+++ UnitTestProject1/CalculatorTest.cs
@@ -1,23 +1,47 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using CalculatorKata;
 
 namespace CalculatorKataTest
 {
     [TestFixture]
     public class CalculatorTest
-
     {
+        private Calculator calculator;
+
         [SetUp]
         public void Init()
         {
-            
+            calculator = new Calculator();
         }
 
         [Test]
-        public void TestMethod1()
+        public void TestEmpty()
         {
-
+            Assert.AreEqual(calculator.Add(""), 0);
         }
+
+        [Test]
+        public void TestOneNumber()
+        {
+            Assert.AreEqual(calculator.Add("1"), 1);
+        }
+
+        [TestCase(3, "1,2")]
+        [TestCase(12, "5,7")]
+        [TestCase(317, "237, 80")]
+        public void TestTwoNumbers(int number, String numberString)
+        {
+            Assert.AreEqual(number, calculator.Add(numberString));
+        }
+
+        [TestCase(6, "1,2,3")]
+        [TestCase(20, "7,5,8")]
+        [TestCase(371, "100,250,21")]
+        public void TestThreeNumbers(int number, String numberString)
+        {
+            Assert.AreEqual(number, calculator.Add(numberString));
+        }
+
     }
 }
