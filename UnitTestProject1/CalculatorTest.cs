c097a173b3bbe1902339781ecdb01169c49dd20d
jtkuhn <jtkuhn@foxguardsolutions.com>
2015-04-27 08:45:11 -0400

    Set up and ready to start coding

---
+++ UnitTestProject1/CalculatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace CalculatorKataTest
+{
+    [TestFixture]
+    public class CalculatorTest
+
+    {
+        [SetUp]
+        public void Init()
+        {
+            
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
