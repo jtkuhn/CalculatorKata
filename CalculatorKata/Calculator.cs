b47f6fe552bacc569eaae21df5a1aa0db768f559
jtkuhn <jtkuhn@foxguardsolutions.com>
2015-04-28 15:35:21 -0400

    More or less finished through step 7ish

--- CalculatorKata/Calculator.cs
+++ CalculatorKata/Calculator.cs
@@ -1,43 +1,79 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace CalculatorKata
 {
     public class Calculator
     {
-        private int sum = 0;
+        private int _sum = 0;
+
+        string _delimiter = "\n";
 
         public int Add(String numbers)
         {
-            sum = 0;
+            CheckForNegatives(numbers);
+            _sum = 0;
+            numbers = SetNewDelimiter(numbers);
 
-            while (numbers.Contains(","))
+            while (numbers.Contains(",") || numbers.Contains("\n") || numbers.Contains(_delimiter))
             {
                 numbers = AddNextNumberToSum(numbers);
             }
             AddZeroOrOneNumberToSum(numbers);
 
-            return sum;
+            return _sum;
         }
 
+        private String SetNewDelimiter(string s)
+        {
+            if (s.StartsWith("//"))
+            {
+                int pos = 2;
+                while (s.Substring(pos, 1) != "\n")
+                {
+                    pos++;
+                }
+                _delimiter = s.Substring(2, pos - 2);
+                return s.Substring(pos + 1);
+            }
+            else return s;
+        }
 
+        private void CheckForNegatives(string s)
+        {
+            if (s.Contains("-"))
+            {
+                throw new Exception("Negatives not allowed");
+            }
+        }
 
         private string AddNextNumberToSum(string s)
         {
             int pos = 0;
-            while (s.Substring(pos, 1) != ",")
+
+            while (s.Substring(pos, 1) != "," && s.Substring(pos, 1) != "\n" && s.Substring(pos, 1) != _delimiter)
             {
                 pos++;
             }
-            sum += Convert.ToInt16(s.Substring(0, pos));
+            _sum += AssertBelow1001(Convert.ToInt16(s.Substring(0, pos)));
             return s.Substring(pos + 1);
+            
+        }
+
+        private int AssertBelow1001(int x)
+        {
+            if (x < 1001)
+                return x;
+            else return 0;
+
         }
 
         private void AddZeroOrOneNumberToSum(string s)
         {
             if (s.Length != 0)
             {
-                sum += Convert.ToInt16(s);
+                _sum += Convert.ToInt16(s);
             }
         }
 
--- UnitTestProject1/CalculatorTest.cs
+++ UnitTestProject1/CalculatorTest.cs
@@ -47,7 +47,39 @@ namespace CalculatorKataTest
         [Test]
         public void TestNewLineSeparator()
         {
-            Assert.AreEqual(5, "1\n4");
+            Assert.AreEqual(5, calculator.Add("1\n4"));
+        }
+
+        [TestCase(15, "1\n2\n3\n4\n5")]
+        [TestCase(15, "1\n2,3,4\n5")]
+        public void TestNewLinesAndCommas(int number, String numberString)
+        {
+            Assert.AreEqual(number, calculator.Add(numberString));
+        }
+
+        [Test]
+        public void TestDifferentDelimiter()
+        {
+            Assert.AreEqual(3, calculator.Add("//;\n1;2"));
+        }
+
+        [Test]
+        public void TestDifferentDelimiterComplex()
+        {
+            Assert.AreEqual(15, calculator.Add("//;\n1;2\n3,4;5"));
+        }
+
+        [Test]
+        public void TestThrowNegativeException()
+        {
+            var ex = Assert.Throws<Exception>(() => calculator.Add("1,-2"));
+            Assert.That(ex.Message, Is.EqualTo("Negatives not allowed"));
+        }
+
+        [Test]
+        public void TestNumbersTooLarge()
+        {
+            Assert.AreEqual(2, calculator.Add("1001, 2005, 17884, 23874\n1384,2"));
         }
 
     }
